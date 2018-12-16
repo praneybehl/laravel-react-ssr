66e78cd4d21f501ce49afecade33cf3c596a9fe6
Shaun Mhonde <mhondeshaun@gmail.com>
2018-12-17 09:32:29 +1100

    back end form validation added - will always return an error for missing form data (middleName)

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,54 +14,74 @@
 */
 
 $welcome = [
-        'login' => Route::has('login'),
-        'register' => Route::has('register'),
-        'formAction' => 'form-submit',
-        'links' => [
-            [ 'id' => 1, 'url' => 'https://laravel.com/docs', 'label' => 'Documentation' ],
-            [ 'id' => 2, 'url' => 'https://laracasts.com', 'label' => 'Laracasts' ],
-            [ 'id' => 3, 'url' => 'https://laravel-news.com', 'label' => 'News' ],
-            [ 'id' => 4, 'url' => 'https://nova.laravel.com', 'label' => 'Nova' ],
-            [ 'id' => 5, 'url' => 'https://forge.laravel.com', 'label' => 'Forge' ],
-            [ 'id' => 6, 'url' => 'https://github.com/laravel/laravel', 'label' => 'GitHub' ],
-            [ 'id' => 7, 'url' => '/form', 'label' => 'Test Form' ],
-            [ 'id' => 8, 'url' => '/form-prefilled', 'label' => 'Test form with Pre-filled Data' ]
-        ]
+    'login' => Route::has('login'),
+    'register' => Route::has('register'),
+    'formAction' => 'form-submit',
+    'links' => [
+        [ 'id' => 1, 'url' => 'https://laravel.com/docs', 'label' => 'Documentation' ],
+        [ 'id' => 2, 'url' => 'https://laracasts.com', 'label' => 'Laracasts' ],
+        [ 'id' => 3, 'url' => 'https://laravel-news.com', 'label' => 'News' ],
+        [ 'id' => 4, 'url' => 'https://nova.laravel.com', 'label' => 'Nova' ],
+        [ 'id' => 5, 'url' => 'https://forge.laravel.com', 'label' => 'Forge' ],
+        [ 'id' => 6, 'url' => 'https://github.com/laravel/laravel', 'label' => 'GitHub' ],
+        [ 'id' => 7, 'url' => '/form', 'label' => 'Test Form' ],
+        [ 'id' => 8, 'url' => '/form-prefilled', 'label' => 'Test form with Pre-filled Data' ]
+    ]
 ];
 
 
 $form = [
-        'formAction' => '/form-submit'
+    'formAction' => '/form-submit'
 ];
 
-Route::get('/', function () use ($welcome){
-    return view('welcome', [ 'welcome' => $welcome ]);
-});
+Route::get(
+    '/',
+    function () use ($welcome) {
+        return view('welcome', [ 'welcome' => $welcome ]);
+    }
+);
 
-Route::get('/form-prefilled', function () {
-    return view('welcome', [ 'welcome' => [
-          'formAction' => '/form-submit',
-          'token' => csrf_token(),
-          'user' => [
-              'firstName' => 'Johny',
-              'lastName' => 'Dunnings',
-              'email' => ''
-          ]
-      ]]);
-});
+Route::get(
+    '/form-prefilled',
+    function () {
+        return view(
+            'welcome',
+            [ 'welcome' => [
+                'formAction' => '/form-submit',
+                'token' => csrf_token(),
+                'user' => [
+                    'firstName' => 'John',
+                    'lastName' => 'Doe',
+                    'email' => 'john@example.com'
+                ]
+            ]
+            ]
+        );
+    }
+);
 
-Route::get('/form', function () {
-    return view('welcome', [ 'welcome' => ['formAction' => '/form-submit', 'token' => csrf_token()] ]);
-});
+Route::get(
+    '/form',
+    function () {
+        return view('welcome', [ 'welcome' => ['formAction' => '/form-submit', 'token' => csrf_token()] ]);
+    }
+);
 
+Route::post(
+    '/form-submit',
+    function (Request $request) {
+        $validateData = $request->validate(
+            [
+                'firstName' => 'required',
+                'middleName' => 'required',
+                'lastName' => 'required',
+                'email' => 'required|email',
+            ]
+        );
 
-Route::post('/form-submit', function () {
-    var_dump($_POST);
-});
-
-
-
-
+        var_dump($validateData);
+    }
+);
 
 Auth::routes();
 
